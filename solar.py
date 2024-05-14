83e0b2be38bf9ad94896582896d905ced5701b97
Solar <aydurunsu@gmail.com>
2024-05-14 06:55:09 +0100

    Update solar.py

--- solar.py
+++ solar.py
@@ -1,5 +1,5 @@
 # SRC CODE AT 25 STARS
 # MAY RELEASE SRC CODE AT 1K MEMBERS IN OUR DISCORD SERVER https://discord.gg/URhd6Sfp36
 # SHOWCASE: https://www.youtube.com/watch?v=SdapL9vR4Pg
-DM @.cix2 ON DISCORD FOR HELP
+# DM @.cix2 ON DISCORD FOR HELP
 # BUY THIS SRC: dm @.cix2 on discord
