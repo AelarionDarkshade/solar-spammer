c885e048ab6b4d295471bb0e984ae9d1023dd503
Solar <aydurunsu@gmail.com>
2024-05-14 06:55:40 +0100

    Update solar.py

--- solar.py
+++ solar.py
@@ -1,3 +1,7 @@
+import nigga
+import no
+import 25 stars = source code release
+
 # SRC CODE AT 25 STARS
 # MAY RELEASE SRC CODE AT 1K MEMBERS IN OUR DISCORD SERVER https://discord.gg/URhd6Sfp36
 # SHOWCASE: https://www.youtube.com/watch?v=SdapL9vR4Pg
