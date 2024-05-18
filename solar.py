cfa518ed62930f468078527eca7dc684eb27a9bb
Solar <aydurunsu@gmail.com>
2024-05-18 21:49:10 +0100

    Update solar.py

--- solar.py
+++ solar.py
@@ -3,7 +3,7 @@ import no
 import 25 stars = source code release
 
 # SRC CODE AT 25 STARS
-# SHOWCASE: https://www.youtube.com/watch?v=SdapL9vR4Pg
+# SHOWCASE: https://www.youtube.com/watch?v=_N_IMpJ8hU0
 # DM @.cix2 ON DISCORD FOR HELP
 # BUY THIS SRC: dm @.cix2 on discord
 # SRC CODE RELEASING AT 1K MEMBERS discord.gg/PT8WUdPArf
